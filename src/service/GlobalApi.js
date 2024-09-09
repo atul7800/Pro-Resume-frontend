43f4683cb9157688ac4ce714b63dc74d9d653f8f
atul <atulgupta.d796@gmil.com>
2024-09-09 18:06:49 +0530

    9-9-24

--- src/service/GlobalApi.js
+++ src/service/GlobalApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 const API_URL = import.meta.env.VITE_API_URL;
 const axiosClient = axios.create({
-    baseURL:API_URL,
+    baseURL:`${API_URL}/api`,
     headers: {
         'Content-Type':'application/json',
         'Authorization':`Bearer ${API_KEY}`
